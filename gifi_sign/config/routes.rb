b10ca0776d8376967b36796ec1b4f1d6c7a7c9e9
lgmoralesa <lgmoralesa@unal.edu.co>
2017-05-30 03:56:37 -0500

    gifi with sign up, sign in and log out

--- gifi_sign/config/routes.rb
+++ gifi_sign/config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  root 	 'main#index'
+  
+  get 	 'main/index'
+  get 	 'signup' => 'users#new'
+  get 	 'login'  => 'sessions#new'
+  post 	 'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :resumees
   resources :files_ups
   resources :members
   resources :groups
   resources :lines
   resources :users
-  get 'main/index'
-  root 'main#index'
-
-
-  get 'signup' => 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
